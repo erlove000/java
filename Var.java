d5b2237c3dd0f4ebe37e1c1b4f21e0b0ee251631
erlove000 <145665604+erlove000@users.noreply.github.com>
2024-02-07 14:43:49 +0000

    variables

---
+++ Var.java
@@ -0,0 +1,12 @@
+class Var{
+    public static void main(String[] args) {
+        //  System.out.println("Hello world");
+        String name = "Rahul";
+        String neighbour = "Love";
+
+        String friend = neighbour;
+
+
+        System.out.println(friend);
+}
+}
