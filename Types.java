b8e6977469a6b67efa54300a7756482fd684809a
erlove000 <145665604+erlove000@users.noreply.github.com>
2024-02-07 14:48:59 +0000

    PRIMITIVE AND NON

---
+++ Types.java
@@ -0,0 +1,16 @@
+class Types{
+    public static void main(String[] args) {
+        //PRIMITIVE TYPE
+        /* BYTE
+        INT
+        LONG
+        CHAR
+        DOUBLE
+        FLOAT 
+        BOOLEAN
+        */
+    //NON PRIMITIVE TYPE
+       String name = "rahul";
+       System.out.println(name.length());
+}
+}
